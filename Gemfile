5e09e96c9278fe33c03683d7dcce985a392ed9ff
Matt <matthv@gmail.com>
2025-03-13 16:10:22 +0100

    feat: add call http over gRPC

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 gem 'sinatra'
-gem 'grpc', '~> 1.71', platforms: [:ruby]
+gem 'httparty'
 gem 'rackup', '~> 2.2'
 gem 'puma', '~> 6.6'
 gem 'sqlite3'
 gem 'sinatra-activerecord'
 gem 'rake'
 
-
 gem 'forest_admin_rpc_agent', path: '/Users/matthieuvideaud/Sites/agents/in-app/agent-ruby/packages/forest_admin_rpc_agent'
-
+gem 'forest_admin_agent', path: '/Users/matthieuvideaud/Sites/agents/in-app/agent-ruby/packages/forest_admin_agent'
+gem 'forest_admin_datasource_customizer', path: '/Users/matthieuvideaud/Sites/agents/in-app/agent-ruby/packages/forest_admin_datasource_customizer'
+gem 'forest_admin_datasource_toolkit', path: '/Users/matthieuvideaud/Sites/agents/in-app/agent-ruby/packages/forest_admin_datasource_toolkit'
--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ Bundler.require
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'json'
+require 'httparty'
 require 'forest_admin_rpc_agent'
 
 require './models/product'
@@ -12,10 +13,15 @@ require './models/order'
 set :database, { adapter: 'sqlite3', database: 'db/development.sqlite3' }
 set :port, 4567
 
-def create_agent
-  ForestAdminRpcAgent::Agent.new(host: 'localhost', port: 50051)
+API_BASE_URL = 'http://localhost:5000'
+
+def create_agent(path)
+  response = HTTParty.get("#{API_BASE_URL}#{path}")
+  return nil unless response.success?
+
+  JSON.parse(response.body)
 rescue StandardError => e
-  puts "[ERROR] Could not create agent - cannot connect to gRPC server: #{e.message}"
+  puts "[ERROR] Could not connect to HTTP server: #{e.message}"
   nil
 end
 
@@ -32,7 +38,7 @@ end
 
 # Home
 get '/' do
-  breadcrumb([['Home', '/']]) +
+  breadcrumb([%w[Home /]]) +
     '<h1>Mini Client RPC</h1>
      <p><a href="/models">Models</a></p>
      <p><a href="/rpc">RPC</a></p>'
@@ -40,7 +46,7 @@ end
 
 # Models
 get '/models' do
-  breadcrumb([['Home', '/'], ['Models', '/models']]) +
+  breadcrumb([%w[Home /], %w[Models /models]]) +
     '<h1>Models</h1>
      <p><a href="/models/products">Products</a></p>
      <p><a href="/models/orders">Orders</a></p>'
@@ -49,7 +55,7 @@ end
 # Products
 get '/models/products' do
   products = Product.all
-  breadcrumb([['Home', '/'], ['Models', '/models'], ['Products', '/models/products']]) +
+  breadcrumb([%w[Home /], %w[Models /models], %w[Products /models/products]]) +
     '<h1>Products</h1>' +
     products.map { |p| "<p>#{p.id}: #{p.name} - $#{p.price}</p>" }.join
 end
@@ -57,7 +63,7 @@ end
 # Orders
 get '/models/orders' do
   orders = Order.includes(:product).all
-  breadcrumb([['Home', '/'], ['Models', '/models'], ['Orders', '/models/orders']]) +
+  breadcrumb([%w[Home /], %w[Models /models], %w[Orders /models/orders]]) +
     '<h1>Orders</h1>' +
     orders.map do |o|
       "<p>Order ##{o.id}: #{o.customer_name} ordered #{o.quantity}x #{o.product&.name}</p>"
@@ -66,18 +72,19 @@ end
 
 ######################### RPC call #########################
 get '/rpc' do
-  breadcrumb([['Home', '/'], ['RPC', '/rpc']]) +
+  breadcrumb([%w[Home /], %w[RPC /rpc]]) +
     '<h1>RPC</h1>
      <p><a href="/rpc/users">users</a></p>'
 end
 
 # Users
 get '/rpc/users' do
-  agent = create_agent
-  return "<h1>Erreur</h1><p>Could not connect to gRPC server.</p>" unless agent
+  users = create_agent('/users')
 
-  users = agent.list_users
-  breadcrumb([['Home', '/'], ['RPC', '/rpc'], ['Users', '/rpc/users']]) +
+  puts "DEBUG users: #{users.inspect}"
+  return "<h1>Error</h1><p>Could not connect to HTTP server.</p>" unless users
+
+  breadcrumb([%w[Home /], %w[RPC /rpc], %w[Users /rpc/users]]) +
     '<h1>User lists</h1>' +
-    users.map { |u| "<p>#{u.id}: #{u.first_name} #{u.last_name} (#{u.email})</p>" }.join
+    users.map { |u| "<p>#{u['id']}: #{u['first_name']} #{u['last_name']} (#{u['email']})</p>" }.join
 end
